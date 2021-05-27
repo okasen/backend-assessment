055d6ba167e756494587d35df0e81b2dbb13d2e8
Jennifer Black <jenniferblack@LDN-32051.local>
2021-05-27 14:42:23 +0100

    minor changes, changed name of FullNameCreator to FullNameFieldGenerator

--- tasks.py
+++ tasks.py
@@ -3,6 +3,7 @@ from typing import Dict
 import datetime
 from dateutil.relativedelta import *
 
+
 def load_records(filepath):
     with open(filepath) as user_records:
         loaded_records = json.load(user_records)
@@ -10,8 +11,8 @@ def load_records(filepath):
     return loaded_records
 
 
-class FullNameCreator():
-    def create_full_name(self) -> Dict:
+class FullNameFieldGenerator:
+    def create_full_name(self) -> list[Dict]:
         for record in self.loaded_records:
             record["full_name"] = record["forename"] + " " + record["surname"]
 
@@ -21,14 +22,14 @@ class FullNameCreator():
         self.loaded_records = loaded_records
 
 
-class ThirtyPlusOnly():
+class ThirtyPlusOnly:
     def __init__(self, loaded_records, todays_date):
         self.over_thirty_records = []
         self.loaded_records = loaded_records
         self.todays_date_full = todays_date
         self.age_to_test = 30
 
-    def collect_over_thirty(self) -> list:
+    def collect_over_thirty(self) -> list[Dict]:
         for record in self.loaded_records:
             date_of_birth = datetime.datetime.strptime(record["date_of_birth"], '%Y/%m/%d').date()
             age_in_years = relativedelta(self.todays_date_full, date_of_birth).years

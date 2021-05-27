08ec05e0fb3cd4d9520ef556298f3c27e9c1babb
Jennifer Black <jenniferblack@LDN-32051.local>
2021-05-27 14:26:21 +0100

    refactored task 2 beyond Green utilising relativedelta

--- tasks.py
+++ tasks.py
@@ -1,7 +1,7 @@
 import json
 from typing import Dict
 import datetime
-
+from dateutil.relativedelta import *
 
 def load_records(filepath):
     with open(filepath) as user_records:
@@ -26,23 +26,13 @@ class ThirtyPlusOnly():
         self.over_thirty_records = []
         self.loaded_records = loaded_records
         self.todays_date_full = todays_date
-        self.today_year = int(self.todays_date_full.year)
-        self.today_month = int(self.todays_date_full.month)
-        self.today_day = int(self.todays_date_full.day)
         self.age_to_test = 30
 
     def collect_over_thirty(self) -> list:
-        print(self.today_year)
         for record in self.loaded_records:
-            split_dates = record["date_of_birth"].split("/")
-            if int(split_dates[0]) < self.today_year - self.age_to_test:
+            date_of_birth = datetime.datetime.strptime(record["date_of_birth"], '%Y/%m/%d').date()
+            age_in_years = relativedelta(self.todays_date_full, date_of_birth).years
+            if age_in_years >= self.age_to_test:
                 self.over_thirty_records.append(record)
-            elif int(split_dates[0]) == self.today_year - self.age_to_test:
-                if int(split_dates[1]) < self.today_month:
-                    self.over_thirty_records.append(record)
-                elif int(split_dates[1]) == self.today_month:
-                    if int(split_dates[2]) <= self.today_day:
-                        self.over_thirty_records.append(record)
 
         return self.over_thirty_records
-
